e91ffebcd7f59d6228ec9b5c373c4bbbf6fbdeb4
Scott Hardy <scott.the.hardy@gmail.com>
2013-01-14 19:09:33 -0800

    added calendar to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    # calculate min and max hours for each day
+    @hour_min = (Course.where('time_start > 400').minimum('time_start') / 100).floor
+    @hour_max = (Course.maximum('time_start') / 100).ceil
+    @hours = (@hour_min..@hour_max).to_a
+    @days = {:M => 'Monday', :T => 'Tuesday',:W => 'Wednesday',:R => 'Thursday',:F => 'Friday',:S => 'Saturday',:U => 'Sunday',}
+
+
   end
 end
