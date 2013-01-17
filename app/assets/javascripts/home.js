1fe742d186583587a8c78c86d2a766e65cc2e668
Scott Hardy <scott.the.hardy@gmail.com>
2013-01-17 01:18:02 -0800

    added calendar rendering when looking up classes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,7 +13,11 @@ $(function() {
                             courses: courses,
                         }
 
-                        renderBars('#courses-added-template', context, '#courses-added');
+                        generateSchedule(courses);
+                        checkOverlap(courses);
+                        drawCalendar(courses);
+
+                        $('#courses-added').html(renderBars('#courses-added-template', context));
                     },
                 });
             }
@@ -28,8 +32,135 @@ $(function() {
 });
 
 
-function renderBars(template_id, context, target) {
+
+
+
+function generateSchedule(courses) {
+    courses.forEach(function(course) {
+        course.schedule = {};
+        var days = course.days.split('');
+        ['M','T','W','R','F','S','U'].forEach(function(day) {
+            course.schedule[day] = (_.contains(days, day)) ? true : false;
+        });
+    });
+}
+
+
+function checkOverlap(courses) {
+    // output
+    var conflicts = [];
+
+    // make unlinked copy of courses
+    courses = courses.slice(0);
+
+    // for each course
+    while (courses.length > 0) {
+        var course_test = courses.shift();
+        console.log('testing', course_test.id);
+        //var overlaps = false;
+
+        // test against each remaining course
+        courses.forEach(function(course) {
+            var overlaps = false;
+
+            // test if starts during
+            if (course_test.time_start >= course.time_start && course_test.time_start <= course.time_end) {
+                console.log(course_test.id, 'starts during', course.id);
+                overlaps = true;
+            }
+
+            // test if ends during
+            if (course_test.time_end >= course.time_start && course_test.time_end <= course.time_end) {
+                console.log(course_test.id, 'ends during', course.id);
+                overlaps = true;
+            }
+
+            // test if starts before and ends after
+            if (course_test.time_start <= course.time_start && course_test.time_end >= course.time_end) {
+                console.log(course_test.id, 'starts before and ends after', course.id);
+                overlaps = true;
+            }
+
+            // test if don't share a common day
+            if (_.intersection( course_test.days.split(''), course_test.days.split('')).length == 0) {
+                console.log(course_test.id, 'doesnt share a day with', course.id);
+                overlaps = false;
+            }
+
+            // if overlaps, add to conflicts
+            if (overlaps) {
+                conflicts.push(course_test.id + ' conflicts with ' + course.id);
+            }
+        });
+    }
+    console.log(conflicts);
+}
+
+
+
+function pad(time) {
+    return ((time.toString().length == 1) ? ('0'+time) : (time));
+}
+
+
+
+
+function renderBars(template_id, context) {
     var source = $(template_id).html();
     var template = Handlebars.compile(source);
-    $(target).html(template(context));
+    return template(context);
+}
+
+
+
+
+
+
+function drawCalendar(courses) {
+    generateSchedule(courses);
+
+    console.log(courses);
+
+    courses.forEach(function(course) {
+        _.each(course.schedule, function(isclass, day) {
+            console.log(day,isclass);
+            if (isclass) {
+                var time_start = inttimeToObject(course.time_start);
+                var time_end = inttimeToObject(course.time_end);
+
+                course.style = {};
+                course.style.height = 40 * (time_end.hour24 - time_start.hour24 + (time_end.minute - time_start.minute)/60) + 'px';
+                course.style.top = 25 + (40 * (time_start.hour24 - 7 + time_start.minute/60)) + 'px';
+
+                var context = {
+                    course: course,
+                }
+                var render = renderBars('#calendar-course', context);
+                $('#' + day).append(render);
+            }
+        });
+    });
+}
+
+
+$(function() {
+    Handlebars.registerHelper('formatTime', function(time) {
+        var time = inttimeToObject(time);
+        return time.hour + ':' + pad(time.minute) + time.ampm;
+    });
+});
+
+
+
+function inttimeToObject(time) {
+    var time = {
+        time: time,
+        hour24: Math.floor(time / 100),
+    };
+
+    time.hour = (time.hour24 > 12) ? (time.hour24 - 12) : time.hour24;
+    time.minute = time.time - (time.hour24 * 100);
+    time.ampm = (time.hour24 < 12) ? 'am' : 'pm';
+
+    return time;
 }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,26 @@
 class HomeController < ApplicationController
   def index
     # calculate min and max hours for each day
-    @hour_min = (Course.where('time_start > 400').minimum('time_start') / 100).floor
-    @hour_max = (Course.maximum('time_start') / 100).ceil
+    #@hour_min = (Course.where('time_start > 400').minimum('time_start') / 100).floor
+    #@hour_max = (Course.maximum('time_end') / 100).ceil + 1
+    @hour_min = 7
+    @hour_max = 8 + 12
     @hours = (@hour_min..@hour_max).to_a
-    @days = {:M => 'Monday', :T => 'Tuesday',:W => 'Wednesday',:R => 'Thursday',:F => 'Friday',:S => 'Saturday',:U => 'Sunday',}
+    @days = {
+        :M => 'Monday',
+        :T => 'Tuesday',
+        :W => 'Wednesday',
+        :R => 'Thursday',
+        :F => 'Friday',
+        :S => 'Saturday',
+        :U => 'Sunday',
+    }
 
-    # stuff for javascript
+    # javascript course list (array of course.name)
     @js_courses = []
     Course.select('DISTINCT(name)').each do |course|
       @js_courses.push(course.name)
     end
+    @js_courses.sort! {|a,b| /[0-9]+/.match(a).to_s.to_i <=> /[0-9]+/.match(b).to_s.to_i}
   end
 end
