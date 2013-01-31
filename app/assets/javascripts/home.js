cfa148f2b9d1c889433933fc4b39940b317e2ddb
Scott Hardy <scott.the.hardy@gmail.com>
2013-01-31 04:17:34 -0800

    now using angular for dom management instead of handlebars/jquery

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,40 +1,3 @@
-$(function() {
-    $('#course-add').typeahead({
-        source: js_courses,
-        items: 10,
-        updater: function(item) {
-            if (_.contains(js_courses, item)) {
-                $.ajax({
-                    url: '/admin/course/' + item + '.json',
-                    success: function(courses) {
-                        console.log(courses)
-
-                        var context = {
-                            courses: courses,
-                        }
-
-                        generateSchedule(courses);
-                        checkOverlap(courses);
-                        drawCalendar(courses);
-
-                        $('#courses-added').html(renderBars('#courses-added-template', context));
-                    },
-                });
-            }
-        }
-    });
-
-
-    $('.course-evaluation').live('click', function(e) {
-        $('#course-evaluation-form').find('input').val($(this).text());
-        $('#course-evaluation-form').submit();
-    });
-});
-
-
-
-
-
 function generateSchedule(courses) {
     courses.forEach(function(course) {
         course.schedule = {};
@@ -98,61 +61,67 @@ function checkOverlap(courses) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function() {
+    // course typeahead
+    $('[ng-model="addCourseText"]').typeahead({
+        source: js_courses,
+        items: 10,
+        updater: function(item) {
+            // trigger input submit
+            setTimeout(function() {
+                var scope = angular.element(document).scope();
+                scope.$broadcast('submit');
+            },0);
+            return item;
+        }
+    });
+});
+
+
+// course evaluation linkout
+$('.course-evaluation').live('click', function(e) {
+    $('#course-evaluation-form').find('input').val($(this).text());
+    $('#course-evaluation-form').submit();
+});
+
+
+// time pad formatting
 function pad(time) {
     return ((time.toString().length == 1) ? ('0'+time) : (time));
 }
 
 
-
-
-function renderBars(template_id, context) {
-    var source = $(template_id).html();
-    var template = Handlebars.compile(source);
-    return template(context);
-}
-
-
-
-
-
-
-function drawCalendar(courses) {
-    generateSchedule(courses);
-
-    console.log(courses);
-
-    courses.forEach(function(course) {
-        _.each(course.schedule, function(isclass, day) {
-            console.log(day,isclass);
-            if (isclass) {
-                var time_start = inttimeToObject(course.time_start);
-                var time_end = inttimeToObject(course.time_end);
-
-                course.style = {};
-                course.style.height = 40 * (time_end.hour24 - time_start.hour24 + (time_end.minute - time_start.minute)/60) + 'px';
-                course.style.top = 25 + (40 * (time_start.hour24 - 7 + time_start.minute/60)) + 'px';
-
-                var context = {
-                    course: course,
-                }
-                var render = renderBars('#calendar-course', context);
-                $('#' + day).append(render);
-            }
-        });
-    });
-}
-
-
-$(function() {
-    Handlebars.registerHelper('formatTime', function(time) {
-        var time = inttimeToObject(time);
-        return time.hour + ':' + pad(time.minute) + time.ampm;
-    });
-});
-
-
-
-function inttimeToObject(time) {
+// convert stored time to object
+function intTimeToObject(time) {
     var time = {
         time: time,
         hour24: Math.floor(time / 100),
@@ -161,6 +130,71 @@ function inttimeToObject(time) {
     time.hour = (time.hour24 > 12) ? (time.hour24 - 12) : time.hour24;
     time.minute = time.time - (time.hour24 * 100);
     time.ampm = (time.hour24 < 12) ? 'am' : 'pm';
+    time.formatted = time.hour + ':' + pad(time.minute) + time.ampm;
 
     return time;
+}
+
+
+
+
+
+
+
+function courseOptionsController($scope,$http,$window) {
+    $scope.courses = [];
+
+
+    // update model after typeahead submit
+    $('[ng-model="addCourseText"]').change(function(event) {
+        $scope.$apply(function(scope){
+            scope.addCourseText = event.target.value;
+        });
+    });
+
+
+    // submit listener from typeahead select callback
+    $scope.$on('submit', function(e,a) {
+        // extract course name from input and clear
+        var name = $scope.addCourseText;
+        $('[ng-model="addCourseText"]').val('');
+
+        // add course
+        $scope.addCourse(name);
+    });
+
+
+    // add course details via ajax
+    $scope.addCourse = function(name) {
+        // request courses and add to model
+        $http.get('/admin/course/' + name + '.json').
+            success(function(courses) {
+                console.log(courses);
+                _.each(courses,function(course) {
+                    var course_ids = _.pluck($scope.courses, 'id'); // get ids of already added courses
+                    if (!_.contains(course_ids, course.id)) {
+                        console.log($window);
+                        course.time_start = $window.intTimeToObject(course.time_start);
+                        course.time_end = intTimeToObject(course.time_end);
+
+                        _.extend(course,{
+                            show: true,
+                            style: $scope.courseCalStyle(course)
+                        });
+                        $scope.courses.push(course);
+                    }
+                });
+            });
+    };
+
+
+    // calculate course position on calendar
+    $scope.courseCalStyle = function(course) {
+        var style = {};
+
+        style.top = 25 + (40 * (course.time_start.hour24 - 7 + course.time_start.minute/60)) + 'px';
+        style.height = 40 * (course.time_end.hour24 - course.time_start.hour24 + (course.time_end.minute - course.time_start.minute)/60) + 'px';
+
+        return style;
+    };
 }
