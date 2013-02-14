535ba17065db5dff6c4cead8872e4bc56b3d66fd
Scott Hardy <scott.the.hardy@gmail.com>
2013-02-13 19:44:29 -0800

    bugfix for selecting courses, grouped by name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,10 +18,10 @@ class HomeController < ApplicationController
 
     # javascript course list (array of course.name)
     @js_courses = []
-    Section.select('fullname, name').group('name').each do |course|
-      @js_courses.push(course.name + ' - ' + course.fullname)
+    Section.select('name').uniq.each do |course|
+      @js_courses.push(course.name + ' - ' + Section.select('fullname').where('name = ?', course.name).first.fullname)
     end
-    @js_courses.sort! {|a,b| /[0-9]+/.match(a).to_s.to_i <=> /[0-9]+/.match(b).to_s.to_i}
+    @js_courses.sort! {|a,b| /[0-9]+/.match(a).to_s.to_i + ((/[0-9]+[A-Z]/.match(a)) ? 0.5 : 0) <=> /[0-9]+/.match(b).to_s.to_i + ((/[0-9]+[A-Z]/.match(b)) ? 0.5 : 0)}
 
     # last updated
     @lastupdated = ((Time.now - Section.first.updated_at) / 60).ceil
