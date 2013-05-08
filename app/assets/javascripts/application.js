8ba292f3eaf0ed867c8ffde7132358c2421ea132
Scott Hardy <scott.the.hardy@gmail.com>
2013-05-07 18:32:34 -0700

    reorganized js files into lib folder and angular app folder, turned on js precompiling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,19 @@
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require bootstrap
+
+// libraries
+//= require lib/ga.js
+//= require lib/store.js
+//= require lib/underscore.js
+
+// angular app dependencies
+//= require core.js
+
+// angular
+//= require lib/angular.min.js
+//= require lib/angular-strap.js
+//= require_tree ./app
+
+// everything else
+//= require_tree .
