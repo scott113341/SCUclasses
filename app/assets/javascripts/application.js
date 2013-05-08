f2b74755ee6ab2fac883416b8cb853c50dab8cfb
Scott Hardy <scott.the.hardy@gmail.com>
2013-05-07 22:04:22 -0700

    added js library versioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,15 +4,17 @@
 
 // libraries
 //= require lib/ga.js
-//= require lib/store.js
-//= require lib/underscore.js
+//= require lib/store-1.3.7.min.js
+//= require lib/underscore-1.4.4.js
 
 // angular app dependencies
 //= require app/core.js
 
-// angular
-//= require lib/angular.min.js
-//= require lib/angular-strap.js
+// angular library
+//= require lib/angular-1.0.4.min.js
+//= require lib/angularstrap-0.7.3.js
+
+// angular app
 //= require app/app.js
 //= require_tree ./app
 
