62877605f5d4cc3178eb15d94d3ab59348483288
Scott Hardy <scott.the.hardy@gmail.com>
2013-07-21 11:03:33 -0600

    organized application.js require statements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,17 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
+// rails gem libraries
+  //= require jquery
+  //= require jquery_ujs
+  //= require bootstrap
 
 // libraries
-//= require lib/ga.js
-//= require lib/store-1.3.7.min.js
-//= require lib/underscore-1.4.4.js
-
-// angular app dependencies
-//= require app/core.js
-
-// angular library
-//= require lib/angular-1.0.4.min.js
-//= require lib/angularstrap-0.7.3.js
+  //= require app/core.js
+  //= require lib/angular-1.0.4.min.js
+  //= require lib/angularstrap-0.7.3.js
+  //= require_tree ./lib
 
 // angular app
-//= require app/app.js
-//= require_tree ./app
+  //= require app/app.js
+  //= require_tree ./app
 
-// everything else
-//= require_tree .
+// anything else
+  //= require_tree .
