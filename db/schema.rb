27572be4dbed93692f109806e461432f8104db4b
Scott Hardy <scott.the.hardy@gmail.com>
2014-09-10 14:11:32 -0600

    add default field to Term model (should be the default term?)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140628013309) do
+ActiveRecord::Schema.define(:version => 20140910195541) do
 
   create_table "cores", :force => true do |t|
     t.string   "key"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20140628013309) do
     t.integer  "number"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "default"
   end
 
 end
